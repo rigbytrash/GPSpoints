8be8c406c8b7245a41c7ba708f24d969d31fdee0
rigbytrash <69912123+rigbytrash@users.noreply.github.com>
2023-10-05 11:43:22 +0100

    added files from private uni Github

---
+++ src/main/java/Track.java
@@ -0,0 +1,41 @@
+/**
+ * Represents a point in space and time, recorded by a GPS sensor.
+ *
+ * @author HAMZA SIDDIQUE
+ */
+public class Track {
+  public Track(){
+
+  }
+
+  public void readFile(String filename){
+
+  }
+
+  public void add(Point point){
+
+  }
+
+  public Point get(int index){
+    return null;
+  }
+  public int size(){
+    return 0;
+  }
+
+  public Point lowestPoint(){
+    return null;
+  }
+
+  public Point highestPoint(){
+    return null;
+  }
+
+  public double totalDistance(){
+    return 0;
+  }
+
+  public double averageSpeed(){
+    return 0;
+  }
+}
